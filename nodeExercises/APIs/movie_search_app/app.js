1f3806f0977ed08132fee5dddcc2cd5774bd5214
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-01-09 12:37:55 -0500

    updated querty string in app to accept user seacrh input; added a search.ejs file that allows the user to submit a search term; updated results.ejs to include year of movie title and with notes to self

--- nodeExercises/APIs/movie_search_app/app.js
+++ nodeExercises/APIs/movie_search_app/app.js
@@ -4,8 +4,13 @@ var express = require("express"),
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("search");
+});
+
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=georgia", function(error, response, body){
+    var query = req.query.search;
+    request("http://www.omdbapi.com/?apikey=thewdb&s=" + query, function(error, response, body){
           if(!error && response.statusCode == 200){
               var searchResults = JSON.parse(body);
               res.render("results", {returnData: searchResults});
