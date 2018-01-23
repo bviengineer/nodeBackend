92b789099fb45aea90a377cda6ec0cf0b7c4849f
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-01-23 14:14:48 -0500

    added two new routes, blogs/new and /blogs

--- nodeExercises/RESTfulRoutingBlog/app.js
+++ nodeExercises/RESTfulRoutingBlog/app.js
@@ -31,6 +31,7 @@ app.get("/", function(req, res){
     res.redirect("/blogs");
 });
 
+//INDEX route
 app.get("/blogs", function(req, res ){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -42,7 +43,23 @@ app.get("/blogs", function(req, res ){
     
 });
 
+//NEW route
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
 
+//CREATE route
+app.post("/blogs", function(req, res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else
+        res.redirect("/blogs");
+    });
+});
+
+//
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING");
 });
