e005db38fc7caaaa3d2376c8c05ef8b62f7d1117
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-02-12 13:56:06 -0500

    included code to sanitize input when a new post is created, in the app.post (create route)

--- nodeExercises/RESTfulRoutingBlog/app.js
+++ nodeExercises/RESTfulRoutingBlog/app.js
@@ -67,6 +67,7 @@ app.get("/blogs/new", function(req, res){
 //CREATE route
 app.post("/blogs", function(req, res){
     //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
