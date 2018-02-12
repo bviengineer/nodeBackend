431f2cd01317854681d01a6c17474ec1dc8db16c
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-02-12 11:43:21 -0500

    added a delete and update route to app.js

--- nodeExercises/RESTfulRoutingBlog/app.js
+++ nodeExercises/RESTfulRoutingBlog/app.js
@@ -1,13 +1,15 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override");
     
 //APP CONFIG    
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use( methodOverride("_method"));
 
 
 //MONGOOSE/MODEL CONFIG 
@@ -65,12 +67,45 @@ app.get("/blogs/:id", function(req, res){
         if(err){
             res.redirect("/blogs");
         } else{
-            res.render("show", {blogContainer: foundBlog});
-        }
+            res.render("show", {blogsContainer: foundBlog});        }
     });
 }); 
 
-//
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.render("edit", {blogsContainer: foundBlog});
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    //destroy blog 
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs"); //redirect somewhere
+        }
+    });
+});
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING");
 });
