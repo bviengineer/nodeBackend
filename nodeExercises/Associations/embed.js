002e2232d47c728c5106ab5f0f0e0924920fb86a
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-03-05 12:35:55 -0500

    addition of an array to the userSchema labeled posts and being passed in postSchema | postSchema relocated to above userSchema

--- nodeExercises/Associations/embed.js
+++ nodeExercises/Associations/embed.js
@@ -8,15 +8,18 @@ mongoose.connect("mongodb://localhost/blog_demo");
 a. user: email, name
 b. POST: title, content
 */
-var userSchema = new mongoose.Schema({
-    name: String,
-    email: String
-});
 
+//postSchema listed above userSchema b/c userSchema is referencing postSchema in its post key/value pair which needs to be declared first
 var postSchema = new mongoose.Schema({
     title: String,
     content: String
 });
+var userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    posts: [postSchema]
+});
+
 
 //4. create DATA model from the schema
 var User = mongoose.model("User", userSchema); //User will be the name of the table while userSchema will be the model it uses to create the User table
