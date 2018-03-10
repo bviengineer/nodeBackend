416ce7bfcbbc1683b73efcf9ed435d077ea187a6
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-03-09 19:25:51 -0500

    created a user and a new post not yet associated

--- nodeExercises/Associations/references.js
+++ nodeExercises/Associations/references.js
@@ -1,3 +1,5 @@
+//SECTION OF VIDE DISCUSSES HOW TO CONNECT A USER TO A POST USING OBJECT REFERENCE VS. BEING HARD CODED
+
 //1. require a database
 var mongoose = require("mongoose");
 
@@ -28,7 +30,20 @@ var userSchema = new mongoose.Schema({
         ]
 });
 
+
 //4. create DATA model from the schema
 var User = mongoose.model("User", userSchema); //User will be the name of the table while userSchema will be the model it uses to create the User table
 var Post = mongoose.model("Post", postSchema); //Post will be the name of the table while postSchema will be the model it uses to create the Post table
 
+//adding a user
+User.create({
+    email: "bob@gmail.com",
+    name: "Bob Belcher"
+});
+
+Post.create({
+    title: "How to cook the best burger",
+    content: "etc etc etc etc etc"
+}, function(err, post){
+    console.log(post);
+});
