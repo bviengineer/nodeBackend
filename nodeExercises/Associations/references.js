d17aa36bd12c52a7be2c1b95efa6f5a4eb0045d2
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-04-21 19:18:59 -0400

    added code to Post.create that will fint the user and push the post to their array and then log the post to the console

--- nodeExercises/Associations/references.js
+++ nodeExercises/Associations/references.js
@@ -45,5 +45,18 @@ Post.create({
     title: "How to cook the best burger",
     content: "etc etc etc etc etc"
 }, function(err, post){
-    console.log(post);
-});
+    User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+       if(err){
+           console.log(err);
+       } else {
+           foundUser.posts.push(post);
+           foundUser.save(function(err, data){
+               if(err){
+                console.log(err);    
+               } else {
+               console.log(data);
+               }
+            });
+          }
+       });
+    });
